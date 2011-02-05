87f738ebaffc2f9e57e38c4e00cbc9c03bdd017d
Jeff Watkins <jeff@metrocat.org>
2011-02-05 12:25:35 -0800

    Oops. Need to order properties correctly.

--- src/model.js
+++ src/model.js
@@ -4,3 +4,7 @@
 /*jsl:import model/ModelObject.js*/
 /*jsl:import model/Model.js*/
 /*jsl:import model/ModelClassMethods.js*/
+/*jsl:import model/Property.js*/
+/*jsl:import model/ToOne.js*/
+/*jsl:import model/ToMany.js*/
+
