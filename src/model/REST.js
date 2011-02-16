1c7075fc60ad45997f3cd6ca8dbd9a3fca98f0cb
Jeff Watkins <jeff@metrocat.org>
2011-02-15 21:07:37 -0800

    Prefetch feature was causing trouble in native environment. Need to revisit this some time.

--- src/model/REST.js
+++ src/model/REST.js
@@ -12,17 +12,17 @@ define("coherent", function(coherent)
     return resource.replace(PARAM_REGEX, function(match, key, keypath)
     {
       var value;
-      var keyname= (key||keypath);
-      var slash= ('/'===keyname.charAt(0));
+      var keyname = (key || keypath);
+      var slash = ('/' === keyname.charAt(0));
       if (slash)
-        keyname= keyname.slice(1);
-        
+        keyname = keyname.slice(1);
+
       if (object.valueForKey)
-        value= key ? object.valueForKey(keyname) : object.valueForKeyPath(keyname);
+        value = key ? object.valueForKey(keyname) : object.valueForKeyPath(keyname);
       else
-        value= key ? object[keyname] : Object.get(object, keyname);
-      
-      if (slash && '/'!==value.charAt(0))
+        value = key ? object[keyname] : Object.get(object, keyname);
+
+      if (slash && '/' !== value.charAt(0))
         return '/' + value;
       else
         return value;
@@ -31,91 +31,70 @@ define("coherent", function(coherent)
 
   coherent.REST = Class.create({
 
-      XHR_OPTIONS: {
-        allowCache: true,
-        responseContentType: 'application/json'
-      },
-      PREFETCH_OPTIONS: {
-        allowCache: false,
-        responseContentType: 'application/json'
-      },
+    XHR_OPTIONS: {
+      allowCache: true,
+      responseContentType: 'application/json'
+    },
+    PREFETCH_OPTIONS: {
+      allowCache: false,
+      responseContentType: 'application/json'
+    },
 
-      constructor: function(model, params)
+    constructor: function(model, params)
+    {
+      this.model = model;
+      this.__prefetches = {};
+      Object.extend(this, params);
+    },
+
+    /**
+      coherent.REST#extractObject(json) -> Object
+    
+      - json (Object): The data retrieve from the server.
+    
+      Sometimes the server doesn't return the resource directly, but wraps it in
+      an envelope or you need to perform some other processing before it's ready
+      for passing to the Model's constructor. Override this method (usually in
+      the parameters when you're defining the REST resource) and you can massage
+      the data.
+     */
+    extractObject: function(json)
+    {
+      return json;
+    },
+
+    cachePrefetch: function(id, data)
+    {
+      return data;
+    },
+
+    prefetch: function(object)
+    {
+      var id = object.valueForKey ? object.valueForKey('id') : Object.get('id');
+      var url = pathFromStringByReplacingParameters(this.resource, object);
+      return XHR.get(url, null, this.PREFETCH_XHR_OPTIONS || this.XHR_OPTIONS);
+    },
+
+    fetch: function(object)
+    {
+      //  Try to use the earlier prefetch deferred value if it's still around
+      var id = object.valueForKey ? object.valueForKey('id') : Object.get('id');
+      var url = pathFromStringByReplacingParameters(this.resource, object);
+      var d = XHR.get(url, null, this.XHR_OPTIONS);
+      d.addCallback(this.extractObject, this);
+      return d;
+    },
+
+    __factory__: function(params)
+    {
+      var klass = this;
+      return function()
       {
-        this.model = model;
-        this.__prefetches = {};
-        Object.extend(this, params);
-      },
+        return new klass(this, params);
+      };
+    }
 
-      /**
-        coherent.REST#extractObject(json) -> Object
-      
-        - json (Object): The data retrieve from the server.
-      
-        Sometimes the server doesn't return the resource directly, but wraps it in
-        an envelope or you need to perform some other processing before it's ready
-        for passing to the Model's constructor. Override this method (usually in
-        the parameters when you're defining the REST resource) and you can massage
-        the data.
-       */
-      extractObject: function(json)
-      {
-        return json;
-      },
-
-      cachePrefetch: function(id, data)
-      {
-        return data;
-      },
-
-      prefetch: function(object)
-      {
-        var id = object.valueForKey ? object.valueForKey('id') : Object.get('id');
-        var url = pathFromStringByReplacingParameters(this.resource, object);
-        var d = XHR.get(url, null, this.PREFETCH_XHR_OPTIONS || this.XHR_OPTIONS);
-
-        // function oncomplete(json)
-        // {
-        //   delete this.__prefetches[id];
-        //   return this.cachePrefetch(id, json);
-        // }
-        // function onfailed(error)
-        // {
-        //   delete this.__prefetches[id];
-        //   return error;
-        // }
-        // d.addCallback(oncomplete, this);
-        // d.addErrorHandler(onfailed, this);
-        // this.__prefetches[object.id] = d;
-        return d;
-      },
-
-      fetch: function(object)
-      {
-        //  Try to use the earlier prefetch deferred value if it's still around
-        var id = object.valueForKey ? object.valueForKey('id') : Object.get('id');
-        var d = this.__prefetches[id];
-
-        if (!d)
-        {
-          var url = pathFromStringByReplacingParameters(this.resource, object);
-          d = XHR.get(url, null, this.XHR_OPTIONS);
-        }
-
-        d.addCallback(this.extractObject, this);
-        return d;
-      },
-
-      __factory__: function(params)
-      {
-        var klass = this;
-        return function()
-        {
-          return new klass(this, params);
-        };
-      }
-
-    });
+  });
 
   coherent.__export('REST');
 
